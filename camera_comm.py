c2d4d292291d97d60e9d888fc0e641b9915795db
ultrongr <ktsambras@gmail.com>
2024-12-03 13:19:13 +0200

    more changes

--- camera_comm.py
+++ camera_comm.py
@@ -15,6 +15,8 @@ detector = htm.handDetector(detectionCon=0.75, )
 fingerlist = []
 inputlist = []
 num = 0
+np.random.seed(0)
+font = cv.FONT_HERSHEY_PLAIN
 
 
 def is_finger_closed(finger_list, direction=1, axis=1):
@@ -38,7 +40,7 @@ def get_closed_fingers(lmlist, direction=1, axis=1):
 
 
 def finger_combo(lmlist):
-    finger_statuses = get_closed_fingers(lmlist, -1, 1)
+    finger_statuses = get_closed_fingers(lmlist, 1, 1)
 
     if finger_statuses == [False, False, True, True]:
         return "Scissors"
@@ -69,9 +71,33 @@ def check_locked_gesture(past_gestures, limit=60):
     else:
         return list(unique_gestures.keys())[0]
 
+def bot_choice():
+    return np.random.choice(["Rock", "Paper", "Scissors"])
+
+def check_winner(player_choice, bot_choice):
+    if player_choice == bot_choice:
+        return "Draw"
+    elif player_choice == "Rock" and bot_choice == "Scissors":
+        return "Player"
+    elif player_choice == "Scissors" and bot_choice == "Paper":
+        return "Player"
+    elif player_choice == "Paper" and bot_choice == "Rock":
+        return "Player"
+    else:
+        return "Bot"
+    
+def play_round(player_choice, bot_choice):
+    print(f"Player: {player_choice}")
+    print(f"Bot: {bot_choice}")
+    winner = check_winner(player_choice, bot_choice)
+    print(f"Winner: {winner}")
+    return winner
+
 
 counter = 0
 past_gestures = []
+start_time = time.time()
+_bot_choice = None
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -79,21 +105,27 @@ while True:
 
     if len(lmlist) != 0:
         counter += 1
-        # time.sleep(3)
         fingerpos1 = finger_combo(lmlist)
         fingerlist.append(fingerpos1)
         past_gestures.append(fingerpos1)
 
         if counter % 10 == 0:
-            print(check_locked_gesture(past_gestures))
+            print(check_locked_gesture(past_gestures, limit = 5))
 
         # print(fingerpos1)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv.putText(img, "yolo", (150, 150), cv.FONT_HERSHEY_PLAIN, 2, (137, 0, 255), 2)
-    cv.putText(img, f'FPS: {int(fps)}', (50, 50), cv.FONT_HERSHEY_PLAIN, 2, (137, 0, 255), 2)
+    # cv.putText(img, "yolo", (150, 150), cv.FONT_HERSHEY_PLAIN, 2, (137, 0, 255), 2)
+    if 0<time.time() - start_time<5:
+        cv.putText(img, f"Game starting in {4-int(time.time() - start_time)}", (150, 150), font, 5, (137, 0, 255), 2)  
+    if 10>time.time() - start_time > 5:
+        if not _bot_choice:
+            _bot_choice = bot_choice()
+        
+        cv.putText(img, _bot_choice, (150, 150), font, 2, (137, 0, 255), 2)
+    cv.putText(img, f'FPS: {int(fps)}', (50, 50), font, 2, (137, 0, 255), 2)
     cv.imshow('Image', img)
     key = cv.waitKey(1)
     if key == ord('v'):
