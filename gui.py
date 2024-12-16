0105d1e706226b6e64055efcacaec7564c3caf77
ultrongr <ktsambras@gmail.com>
2024-12-16 18:41:12 +0200

    works

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ font = cv2.FONT_HERSHEY_PLAIN
 broker = 'test.mosquitto.org'
 port = 1883
 topic = "hci_2024"  
-client_id = 'rand_id' +str(np.random.randint(0,1000))
+client_id = 'rand_id' + str(np.random.randint())
 
 
 def connect_mqtt():
